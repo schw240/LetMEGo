dd9a21c3cb3d38aa1fb19adc220fcefcdca3418f
Kim Hanju <54871612+schw240@users.noreply.github.com>
2020-11-11 12:25:31 +0900

    Delete model_bank.py

--- backend/LetMEGo/model_bank.py
+++
@@ -1,6 +0,0 @@
-class ForeignBank:
-    def __init__(self, _country_name, _USD_buy, _JPY_buy, _UpdateDate):
-        self.country_name = _country_name
-        self.USD_buy = _USD_buy
-        self.JPY_buy = _JPY_buy
-        self.UpdateDate = _UpdateDate
