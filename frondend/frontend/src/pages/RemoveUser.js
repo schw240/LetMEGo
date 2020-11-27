ba6fb21424041976c2089f3fbcb43a5b2ed616d3
hrpark <xhahhaahoox@gmail.com>
2020-11-27 17:34:47 +0900

    회원 탈퇴

--- frondend/frontend/src/pages/RemoveUser.js
+++ frondend/frontend/src/pages/RemoveUser.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 
 import { Form, Card, Button } from "tabler-react";
+import Account from '../Account'
 
-function RemoveUser() {
+function RemoveUser({history}) {
+    const token = window.localStorage.getItem("token");
     //탈퇴 이유
     const [reason, setReason] = React.useState('')
-
     const [radioGroup, setRadioGroup] = React.useState({
         service: true,
         benefits: false,
@@ -14,47 +15,74 @@ function RemoveUser() {
     })
 
     const radioSelect = (e) => {
-        console.log(e.target.value)
         if(e.target.value === 'service'){
-          setRadioGroup({
-            service: true,
-            benefits: false,
-            protect: false,
-            other: false,
+            setRadioGroup({
+                service: true,
+                benefits: false,
+                protect: false,
+                other: false,
             })
+            setReason(null)
         }else if(e.target.value === 'benefits'){
-          setRadioGroup({
-            service: false,
-            benefits: true,
-            protect: false,
-            other: false,
+            setRadioGroup({
+                service: false,
+                benefits: true,
+                protect: false,
+                other: false,
             })
+            setReason(null)
         }else if(e.target.value === 'protect'){
             setRadioGroup({
-              service: false,
-              benefits: false,
-              protect: true,
-              other: false,
+                service: false,
+                benefits: false,
+                protect: true,
+                other: false,
             })
+            setReason(null)
         }else if(e.target.value === 'other'){
             setRadioGroup({
-              service: false,
-              benefits: false,
-              protect: false,
-              other: true,
+                service: false,
+                benefits: false,
+                protect: false,
+                other: true,
             })
         }
     }
-
-    const otherReason = (e) => {
-        setReason(e.target.value)
-    }
     
     const removeSubmit = () => {
-        console.log('탈퇴 이유 버튼~~')
-        console.log(radioGroup)
-        console.log(reason)
-
+        Account.post("withdraw", [{
+            "service": radioGroup.service,
+            "benefits": radioGroup.benefits,
+            "privacy": radioGroup.protect,
+            "other": reason,
+        }])
+        .then(response=>{
+            const {data} = response
+            if(data.result){
+                console.log(token)
+                Account.post("deleteuser", {
+                    headers: {
+                        Authorization: "JWT " + token
+                    }
+                })
+                .then(response=>{
+                  const {data} = response
+                  if(data.result){
+                      alert('성공적으로 탈퇴가 완료되었습니다.')
+                      if (token != null) {
+                        window.localStorage.removeItem("token")
+                      }
+                      history.push('/login')
+                  }
+                })
+                .catch(error=>{
+                    console.error(error)
+                })
+            }
+        })
+        .catch(error=>{
+            console.error(error)
+        })   
     }
 
     return (
@@ -72,7 +100,7 @@ function RemoveUser() {
 
             <Form.Group>
                 <Form.Radio label="기타" value="other" checked={radioGroup.other} onChange={radioSelect}/>
-                <Form.Textarea disabled={!radioGroup.other} name="removeReason" placeholder="탈퇴 이유를 적어주세요.." rows={6} onChange={otherReason}/>
+                <Form.Textarea disabled={!radioGroup.other} name="removeReason" placeholder="탈퇴 이유를 적어주세요.." rows={6} onChange={(e)=>{setReason(e.target.value)}}/>
             </Form.Group>                
             <Form.Footer>
                 <Button type="button" color="primary" onClick={removeSubmit} block={true}>확인</Button>
