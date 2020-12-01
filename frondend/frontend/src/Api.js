4af71252fc78a658654bb8bfa2fccc25e7d0c32d
schw240 <schw240@gmail.com>
2020-12-01 19:57:24 +0900

    서버 변경

--- frondend/frontend/src/Api.js
+++ frondend/frontend/src/Api.js
@@ -2,7 +2,8 @@ import Axios from 'axios'
 
 const api = Axios.create(
 { 
-    baseURL:"http://127.0.0.1:8000/api/",
+    //baseURL:"http://127.0.0.1:8000/api/",
+    baseURL:"http://54.180.112.37/api/",
     timeout: 3000,
 });
 export default api;
